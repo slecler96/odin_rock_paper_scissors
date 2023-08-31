7fac53cf302e230032501657703fdfd9df480bde
slecler96 <simon.lecler96@gmail.com>
2023-08-31 18:43:07 +0200

    Completing the function updateWinner() and checkWinner() Added final comments

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,59 +1,87 @@
-const ROCK = "rock"
-const PAPER = "paper"
-const SCISSORS = "scissors"
+
+/*
+Constants
+*/
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors";
+const MAXSCORE = 5;
 
 
-const playerButtons = document.querySelectorAll('.player_buttons button')
-const computerButtons = document.querySelectorAll('.computer_buttons button')
-const roundOutcomeMessage = document.querySelector('.roundOutcomeMessage')
-const currentScore = document.querySelector('.score')
-const gameOutcomeMessage = document.querySelector('.gameOutcomeMessage')
-const restartButton = document.querySelector('#restartButton')
+const playerButtons = document.querySelectorAll('.player_buttons button');
+const roundOutcomeMessage = document.querySelector('.roundOutcomeMessage');
+const gameOutcomeMessage = document.querySelector('.gameOutcomeMessage');
+const restartButton = document.querySelector('#restartButton');
 
+/*
+Display the score required to win
+*/
+const maxScorehtml = document.querySelector('#maxScore'); 
+maxScorehtml.textContent = MAXSCORE;
+
+/*
+Reload the web page when the user clicks on the restart button
+*/
 restartButton.addEventListener('click',() => location.reload());
 
+
+/*
+Scores
+*/
 let playerScore = 0;
 let computerScore=0;
 
-playerButtons.forEach(button => { button.addEventListener('click', getPlayerSelection) });
+
+/*
+Activate player buttons
+*/
+playerButtons.forEach(button => {button.addEventListener('click', getPlayerSelection)});
 
 
-
+/*
+Play a round of rock paper scissors, display the winner and update the score.
+Then it checks if the player or the computer wins the game by reaching the maximum score
+*/
 function playRound(playerSelection, computerSelection) {
     // returns 1 when the player wins the round, -1 when he loses and 0 and the game is tie
-    let result
-    console.log(computerSelection)
-    if (playerSelection == computerSelection) {
+    let result = 0;
+    if (playerSelection === computerSelection) {
         tieRoundMessage()
         result = 0;
     }
     else {
         switch (playerSelection) {
             case ROCK:
-                if (computerSelection == PAPER) {
+                if (computerSelection === PAPER) {
                     result = -1;
+                    break;
                 }
                 else {
                     result = 1;
+                    break;
                 }
             case PAPER:
-                if (computerSelection == SCISSORS) {
+                if (computerSelection === SCISSORS) {
                     result = -1;
+                    break;
                 }
                 else {
                     result = 1;
+                    break;
                 }
             case SCISSORS:
-                if (computerSelection == ROCK) {
+                if (computerSelection === ROCK) {
                     result = -1;
+                    break;
                 }
                 else {
                     result = 1;
+                    break;
                 }   
         }
+        result === 1 ? winRoundMessage(playerSelection,computerSelection) : loseRoundMessage(playerSelection,computerSelection)
     }
-    console.log("result"+result)
-    result === 1 ? winRoundMessage() : loseRoundMessage()
+
     
     updateScore(result)
     checkWinner()
@@ -83,7 +111,6 @@ function getComputerChoice(event) {
     let max = 3;
     let choice = Math.floor(Math.random() * max);
     let computerChoice = computerChoices[choice]
-    console.log("comp choice "+computerChoice)
     const computerButton = document.querySelector('#'+computerChoice+'Computer');  
     computerButton.style.background = 'red';
     setTimeout(() => {
@@ -93,53 +120,39 @@ function getComputerChoice(event) {
 }
     
 
-
+/*
+Check is the player or the computer reached the maximum score and wins the game
+*/
 function checkWinner() {
-    if (playerScore === 5 || computerScore === 5) {
-        let win = (compScore > playerScore) ? 'computer' : 'player';
-        updateWinner(win);
+    if (playerScore === MAXSCORE || computerScore === MAXSCORE) {
+        let winner = (computerScore > playerScore) ? 'computer' : 'player';
+        updateWinner(winner);
       }
 }
 
 /*
+Check is the player or the computer reached the maximum score and wins the game
+*/
+function updateWinner(winner) {
+    const gameOutcomeMessage = document.querySelector('.gameOutcomeMessage');
+    let message = `${winner} wins! Click restart to play again.`;
+    showText(message, 0, 50)
+    playerButtons.forEach(button => { button.removeEventListener('click', getPlayerSelection) });
+}
 
-
-
-
-function game() {
-    let roundNumber = parseInt(prompt("Choose the number of rounds: "));
-
-    while (isNaN(roundNumber) || roundNumber < 0) {
-        roundNumber = prompt(roundNumber + " is not a valid number. Please enter a positive number");
-    }
-
-    let scoreRound = 0;
-    let scorePlayer = 0;
-    let scoreComputer = 0;
-
-    for (let round = 1; round <= roundNumber; round++) { 
-        let computerSelection = getComputerChoice()
-        let playerSelection = getPlayerSelection()
-        scoreRound = playRound(playerSelection,computerSelection)
-        if (scoreRound == 1) {
-            scorePlayer += 1;
-        }
-        else if (scoreRound == -1) {
-            scoreComputer += 1
-        }
-    }
-
-    if (scorePlayer == scoreComputer) {
-        return "The game is tie. The score is "+scorePlayer + "-" + scoreComputer;
-    }
-    else if (scorePlayer > scoreComputer) {
-        return "You win! The score is " + scorePlayer + "-" + scoreComputer;
-    }
-    else {
-        return "You lose! The score is " + scorePlayer+-"-" + scoreComputer;
+/*
+Show text letter by letter with a specified time interval
+*/
+function showText(message, index, interval) {   
+    const gameOutcomeMessage = document.querySelector('.gameOutcomeMessage');
+    if (index < message.length) {
+        gameOutcomeMessage.append(message[index++]);
+      setTimeout(function () { showText(message, index, interval); }, interval);
     }
 }
 
+/*
+Message displayed when the round is tied
 */
 function tieRoundMessage(){
     const outcomeMessage = document.querySelector('.roundOutcomeMessage');
@@ -147,20 +160,38 @@ function tieRoundMessage(){
 }
 
 
-
-function winRoundMessage(){
+/*
+Message displayed when the player wins the round
+*/
+function winRoundMessage(playerSelection, computerSelection){
     const outcomeMessage = document.querySelector('.roundOutcomeMessage');
-    outcomeMessage.textContent = "You win!";
+    if (playerSelection === SCISSORS) {
+        outcomeMessage.textContent = `${playerSelection} beat ${computerSelection}.  You win!`;
+    }
+    else {
+        outcomeMessage.textContent = `${playerSelection} beats ${computerSelection}.  You win!`;
+    }   
 }
 
-function loseRoundMessage(){
+/*
+Message displayed when the player loses the round
+*/
+function loseRoundMessage(playerSelection, computerSelection){
     const outcomeMessage = document.querySelector('.roundOutcomeMessage');
-    outcomeMessage.textContent = "You lose!";
+    if (computerSelection === SCISSORS) {
+        outcomeMessage.textContent = `${computerSelection} beat ${playerSelection}.  You lose!`;
+    }
+    else {
+        outcomeMessage.textContent = `${computerSelection} beats ${playerSelection}.  You lose!`;
+    }
+    
 
 }
 
 
-
+/*
+Update the score after each round
+*/
 function updateScore(roundOutcome) {
     if (roundOutcome == 1) {
         playerScore += 1;
@@ -168,34 +199,6 @@ function updateScore(roundOutcome) {
     else if (roundOutcome == -1) {
         computerScore += 1;
     }
-    console.log("score"+playerScore)
     const score = document.querySelector('.score p').textContent = `Player ${playerScore} - ${computerScore} Computer`
 }
 
-/* console.log("start")
-const buttons = document.querySelectorAll('button')
-
-
-
-buttons.forEach((button) => {
-    // and for each one we add a 'click' listener
-    button.addEventListener('click', () => {
-        roundOutcome = playRound(button.id,getComputerChoice());
-        roundNumber += 1;
-        updateScore(roundOutcome);
-        displayUpdatedScore(playerScore, computerScore);
-        if (roundNumber == 5) {
-            const gameOutcomeMessage = document.querySelector('#gameOutcomeMessage');
-            if (playerScore > computerScore) {
-                gameOutcomeMessage.textContent = 'The game is over : You win!';
-            }
-            else if (computerScore > playerScore) {
-                gameOutcomeMessage.textContent = 'The game is over : Computer wins!';
-            }
-            else {
-                gameOutcomeMessage.textContent = 'The game is over : It\'s a tie!';
-            }
-        };
-    });
-});
- */
